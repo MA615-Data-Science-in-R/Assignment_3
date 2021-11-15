67ef5c3d7a28d60fa7275c94afa34e7bd49b3185
jcarbaugh7 <carbaugh@bu.edu>
2021-11-15 16:35:33 -0500

    Added testing maps

--- hmap.R
+++ hmap.R
@@ -25,17 +25,17 @@ install.packages("hurricaneexposuredata")
 #Extract the data of Katrina
 Katrina_track <- filter(hurr_tracks, storm_id =="Katrina-2005")
 
-map_counties(storm = "Floyd-1999", metric = "rainfall") +
-    ggtitle("Floyd-1999") +
+map_counties(storm = "Katrina-2005", metric = "rainfall",days_included = c(0)) +
+    ggtitle("Katrina-2005") +
     theme(plot.title = element_text(hjust = 0.5))
 
 
 
-map_rain_exposure(storm ="Allison-2001", 
+map_rain_exposure(storm ="Katrina-2005", 
                   rain_limit = 175, 
                   dist_limit = 500, 
                   days_included =-5:3) +
-    ggtitle("Allison-2001") +
+    ggtitle("Katrina-2005") +
     theme(plot.title = element_text(hjust = 0.5))
 
 
@@ -55,11 +55,11 @@ map_rain_exposure(storm ="Allison-2001",
 map_counties(storm = "Katrina-2005", metric = "wind")
 
 
-map_counties("Katrina-2005", metric = "wind", wind_var = "sust_dur")
+map_counties("Katrina-2005", metric = "wind", wind_var = "vmax_gust", days_included = c(0))
 
 
 
-map_counties("Katrina-2005", metric = "wind", wind_source = "ext_tracks")
+map_counties("Katrina-2005", metric = "wind", wind_var = "vmax_gust", wind_source = "ext_tracks")
 # 
 # 
 # map_counties(storm = "Sandy-2012", metric = "distance")
@@ -81,12 +81,12 @@ map_wind_exposure(storm = "Katrina-2005",
     wind_limit = convert_wind_speed(34, "knots","mps"))
 
 
-map_event_exposure(storm = "Floyd-1999", event_type = "flood")
+map_event_exposure(storm = "Katrina-2005", event_type = "flood",add_track = TRUE)
 
-map_event_exposure(storm = "Ivan-2004", event_type = "tornado")
+map_event_exposure(storm = "Katrina-2005", event_type = "tornado")
 
 
-map_tracks(storms = "Floyd-1999")
+map_tracks(storms = "Katrina-2005")
 
 
 
@@ -106,9 +106,9 @@ map_tracks(storms = storms_2018$storm_id)
 
 
 
-floyd_map <- map_event_exposure(storm = "Floyd-1999", event_type = "flood")
+katrina_map <- map_event_exposure(storm = "Katrina-2005", event_type = "flood")
 
-map_tracks(storms = "Floyd-1999",
+map_tracks(storms = "Katrina-2005",
     plot_object =floyd_map,
     plot_points =TRUE,
     color ="darkgray"
